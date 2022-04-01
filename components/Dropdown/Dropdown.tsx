5d728336bdb114245f1b4c1ddc0c7dd1779f7b8d
Ralph Largo <devlargs@gmail.com>
2022-04-01 09:26:29 +0800

    updated tslint config

---
+++ components/Dropdown/Dropdown.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react';
+import { HeadingTitle } from 'styles/typography';
+
+const Dropdown: FC = () => {
+  return (
+    <>
+      <HeadingTitle>Dropdown</HeadingTitle>
+    </>
+  );
+};
+
+export default Dropdown;
---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+import Dropdown from '@components/Dropdown';
+import { FC } from 'react';
+
+const Home: FC = () => <Dropdown />;
+
+export default Home;
